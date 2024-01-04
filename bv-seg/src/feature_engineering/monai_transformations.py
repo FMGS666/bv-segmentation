1309d5a960ccb8b73fd9d7cc0bc75a41ee15bb74
FMGS666 <b00794799@essec.edu>
2024-01-04 20:26:21 +0100

    error in the transformatiosn

--- bv-seg/src/feature_engineering/monai_transformations.py
+++ bv-seg/src/feature_engineering/monai_transformations.py
@@ -32,15 +32,6 @@ def get_monai_transformations(
         [
             LoadImaged(keys=["image", "label"], ensure_channel_first = True),
             NormalizeIntensityd(keys=["image"]),
-            ScaleIntensityRanged(
-                keys=["label"],
-                a_min=0,
-                a_max=255,
-                b_min=0.0,
-                b_max=1.0,
-                clip=True,
-            ),
-            CropForegroundd(keys=["image", "label"], source_key="image", allow_smaller = False),
             Orientationd(keys=["image", "label"], axcodes="RAS"),
             EnsureTyped(keys=["image", "label"], device=device, track_meta=False),
             RandCropByPosNegLabeld(
@@ -60,7 +51,7 @@ def get_monai_transformations(
             RandFlipd(
                 keys=["image", "label"],
                 spatial_axis=[0],
-                prob=0.10,
+                prob=0.5,
             ),
             RandFlipd(
                 keys=["image", "label"],
@@ -71,11 +62,6 @@ def get_monai_transformations(
                 keys=["image", "label"],
                 spatial_axis=[2],
                 prob=0.5,
-            ),
-            RandRotate90d(
-                keys=["image", "label"],
-                prob=0.5,
-                max_k=3,
             ),
             RandShiftIntensityd(
                 keys=["image"],
@@ -105,20 +91,31 @@ def get_monai_transformations(
             CropForegroundd(keys=["image", "label"], source_key="image", allow_smaller = False),
             Orientationd(keys=["image", "label"], axcodes="RAS"),
             EnsureTyped(keys=["image", "label"], device=device, track_meta=True),
-            RandCropByPosNegLabeld(
+            RandFlipd(
                 keys=["image", "label"],
-                label_key="label",
-                spatial_size=(
-                    spatial_size, 
-                    spatial_size, 
-                    spatial_size
-                ),
-                pos=1,
-                neg=1,
-                num_samples=1,
-                image_key="image",
-                image_threshold=0,
+                spatial_axis=[0],
+                prob=0.5,
             ),
+            RandFlipd(
+                keys=["image", "label"],
+                spatial_axis=[1],
+                prob=0.5,
+            ),
+            RandFlipd(
+                keys=["image", "label"],
+                spatial_axis=[2],
+                prob=0.5,
+            ),
+            RandShiftIntensityd(
+                keys=["image"],
+                offsets=0.1,
+                prob=1.0,
+            ),
+            RandScaleIntensityd(
+                keys=["image"],
+                factors=0.1,
+                prob=1.0,
+            )
         ]
     )
 
@@ -128,11 +125,6 @@ def get_monai_transformations(
             NormalizeIntensityd(keys=["image"]),
             CropForegroundd(keys=["image"], source_key="image", allow_smaller = False),
             Orientationd(keys=["image"], axcodes="RAS"),
-            Spacingd(
-                keys=["image"],
-                pixdim=(1.0, 1.5, 2.0),
-                mode=("bilinear"),
-            ),
             EnsureTyped(keys=["image"], device=device, track_meta=True),
         ]
     )
